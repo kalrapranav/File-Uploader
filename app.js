452fcb81f651e8ad25c690daabd3b27a82a5e879
Pranav kalra <pkalra@sdsu.edu>
2019-07-10 14:01:52 -0700

    Image displaying functionality added

--- app.js
+++ app.js
@@ -28,6 +28,8 @@ const upload = multer({
     //to set the limit of the files uploaded and it is in bytes
     limits: {fileSize: 1000000},
     fileFilter: function(req, file, cb){
+        //to check what file is getting uploaded and in future we can 
+        //alter according to our functnality for specific file type
         checkFileType(file, cb);
     }
 }).single('myImage');
@@ -64,18 +66,52 @@ app.get("/", (req, res) => {
     res.render("index");
 });
 
+// app.post('/upload', (req, res) => {
+//     upload(req, res, (err) => {
+//         if(err){
+//             res.render('index', {
+//                 msg: err
+//             });
+//         } else {
+//             // console.log(req.file);
+//             // res.send('test');
+
+//             // Don't submit the fomr if the user has not submitted
+//             //an image
+//             if(req.file = undefined) {
+//                 res.render('index', {
+//                     msg: 'Error: no file selected'
+//             });
+//             //Display the submitted image on the home page
+//         } else {
+//             res.render('index', {
+//                 msg: 'File Uploaded!',
+//                 file: `uploads/${req.file.filename}`
+//             });
+//         }
+//     }
+//     });
+// });
 app.post('/upload', (req, res) => {
     upload(req, res, (err) => {
-        if(err){
-            res.render('index', {
-                msg: err
-            });
+      if(err){
+        res.render('index', {
+          msg: err
+        });
+      } else {
+        if(req.file == undefined){
+          res.render('index', {
+            msg: 'Error: No File Selected!'
+          });
         } else {
-            console.log(req.file);
-            res.send('test');
+          res.render('index', {
+            msg: 'File Uploaded!',
+            file: `uploads/${req.file.filename}`
+          });
         }
+      }
     });
-});
+  });
  /**
   * This is all the information printed from req.file above
     and can be used in in multiple ways
